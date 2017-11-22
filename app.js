96e2621bb5a34ee4a7ac99eac8ca9eb1b6a95dd0
etochy <etochy@gmail.com>
2017-11-22 10:49:52 +0100

    prise en compte des zones : Tests en local

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var bien = 0;
 var moyen = 0;
 var mal = 0;
 var nav = false;
+var maZone;
+var lat;
+var long;
+var init = false;
+var dataInit = false;
 
 const Foglet = require('foglet').Foglet
 
@@ -34,10 +39,22 @@ app.share()
 app.connection()
 .then(() => {
   console.log('application connected!')
+  /* @deprecated
   var id = app.getRandomNeighbourId();
   if(id !== null)
     app.sendUnicast(id, 'init');
+*/
 
+  //initialisation
+  var id = app.getRandomNeighbourId();
+  if(id !== null){
+    init = true;
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(maPosition);
+    }
+  }else{
+    dataInit = true;
+  }
   afficher();
 
   if(navigator.geolocation)
@@ -45,19 +62,19 @@ app.connection()
   // listen for incoming broadcast
   app.onBroadcast((id, msg) => {
     console.log('I have received a message from peer', id, ':', msg)
-    choix(msg);
-    
+    broad(msg, id);
   })
+  
   
   app.onUnicast((id, msg) => {
     console.log('I have received a message from neighbour peer', id, ':', msg)
-    if(msg == "init"){
+  /*  if(msg == "init"){
       var values = [bien,moyen,mal];
       app.sendUnicast(id,values);
     }
-    else{
+    else{*/
       recupData(msg);
-    }
+    //}
   })
 
   // send our message each time we hit the button
@@ -75,37 +92,74 @@ app.connection()
       navigator.geolocation.getCurrentPosition(maPosition);
       
     choix(x);
-    app.sendBroadcast(x)
+    var y = [x, maZone];
+    app.sendBroadcast(y);
   }, false)
 })
 .catch(console.error) // catch connection errors
 
 function choix(x){
   if(x == "Bien"){
+    bien++;
+  }
+  else if (x == "Moyen"){
+    moyen++;
+  }
+  else{
+    mal++;
+  }
+  afficher();
+}
+function broad(x, id){
+  console.log("ma zone : "+maZone +" ; la sienne : "+x[1]);
+  if (x[1][0] == maZone[0] && x[1][1]== maZone[1]){
+    if(x[0] == "init"){
+        var values = [bien,moyen,mal];
+        app.sendUnicast(id,values);
+    }
+    else if (x[0] == "Bien"){
       bien++;
     }
-    else if (x == "Moyen"){
+    else if (x[0] == "Moyen"){
       moyen++;
     }
     else{
       mal++;
     }
-    afficher();
+  }
+  afficher();
 }
 
 function recupData(msg){
   //console.log(msg.toString());
-  bien += msg[0];
-  moyen += msg[1];
-  mal += msg[2];
-  afficher();
+  if(!dataInit){
+    dataInit = true;
+    bien += msg[0];
+    moyen += msg[1];
+    mal += msg[2];
+    afficher();
+  }
 }
 
 function maPosition(position) {
   var infopos = "Position déterminée :\n";
-  infopos += "Latitude : "+position.coords.latitude +"\n";
-  infopos += "Longitude: "+position.coords.longitude+"\n";
-  infopos += "Altitude : "+position.coords.altitude +"\n";
+  
+  lat = String(position.coords.latitude).split('.');
+  long = String(position.coords.longitude).split('.');
+  maZone = [lat[0]+"."+lat[1].charAt(0),long[0]+"."+long[1].charAt(0)];
+  
+  console.log("ma zone : "+maZone);
+  
+  if (init === true){
+    var x = ["init", maZone];
+    app.sendBroadcast(x);
+    init = false;
+  }
+  
+  infopos += "Latitude : "+lat +"\n";
+  infopos += "Longitude: "+long+"\n";
+
+  //infopos += "Altitude : "+position.coords.altitude +"\n";
   document.getElementById("maPos").innerHTML = infopos;
   //alert(infopos);
 }
